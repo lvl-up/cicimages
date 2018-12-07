1ea24eeecee7d01bf01144f27861d870fab4d90b
Leon Davis <leon.davis@lvl-up.uk>
2018-12-07 15:18:37 +0000

    Version bump to 0.0.39

--- support/bin/sh/functions/checksum.sh
+++ support/bin/sh/functions/checksum.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+
+# shellcheck source=support/bin/sh/functions/utils.sh
+CHECKSUM_SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "${CHECKSUM_SCRIPT_PATH}/functions/utils.sh"
+
 function check_required_enviroment_is_set() {
     exit_unless_var_defined TRACKS_PATH && exit_unless_directory_exists "${TRACKS_PATH}"
     exit_unless_var_defined EXERCISES_PATH && exit_unless_directory_exists "${EXERCISES_PATH}"
